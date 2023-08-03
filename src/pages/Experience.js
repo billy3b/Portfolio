c35ba7106f898523ee3ef52f551825dbced64991
Avinash Roy <108816279+billy3b@users.noreply.github.com>
2023-08-03 22:39:02 +0530

    added experience section

---
+++ src/pages/Experience.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@material-ui/icons/School";
+
+function Experience() {
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2019"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            DAV Public School, Chhal, Chhattisgarh
+          </h3>
+          <p> High School</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2020 - 2024"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            VIT Bhopal University, Madhya Pradesh
+          </h3>
+
+          <h4 className="vertical-timeline-element-subtitle">
+            B.Tech
+          </h4>
+
+          <p> Computer Science with specialization in Cyber Security and Digital Forensics</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
+}
+
+export default Experience;
