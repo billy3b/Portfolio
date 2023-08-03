ad6a866875158c0d7e9e3da53dc3ee1d9eb35501
Avinash Roy <108816279+billy3b@users.noreply.github.com>
2023-08-03 21:46:14 +0530

    added helpers

---
+++ helpers/ProjectList.js
@@ -0,0 +1,32 @@
+import Proj1 from "../assets/proj1.jpg";
+import Proj2 from "../assets/proj2.jpg";
+import Proj3 from "../assets/proj3.jpg";
+import Proj4 from "../assets/proj4.jpg";
+
+
+export const ProjectList = [
+  {
+    name: "Socials",
+    image: Proj1,
+    skills: "NextJS,TailwindCSS,Firebase",
+    link:'https://github.com/billy3b/Socials'
+  },
+  {
+    name: "AudioTrekk-Ecommerce Website",
+    image: Proj2,
+    skills: "NextJS,Sanity,Stripe",
+    link:'https://github.com/billy3b/AudioTrekk'
+  },
+  {
+    name: "Youtube Clone",
+    image: Proj3,
+    skills: "React,TailwindCSS,MongoDB,youtubev3API",
+    link:'https://github.com/billy3b/youtube-clone'
+  },
+  {
+    name: "Recipe App",
+    image: Proj4,
+    skills: "React,TailwindCSS,MongoDB",
+    link: 'https://github.com/billy3b/MERN-Recipes'
+  },
+];
