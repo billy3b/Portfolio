3ad20bfe7da5e4d14adc629344f16e7aba9cee39
Avinash Roy <108816279+billy3b@users.noreply.github.com>
2023-09-13 16:11:45 +0530

    Update ProjectList.js

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -2,7 +2,7 @@ import Proj1 from "../assets/proj1.jpg";
 import Proj2 from "../assets/proj2.jpg";
 import Proj3 from "../assets/proj3.jpg";
 import Proj4 from "../assets/proj4.jpg";
-
+import proj5 from "../assets/proj5.jpg";
 
 export const ProjectList = [
   {
